02fe92ae2f32734228574392339a4968db76f5e7
Andrew Doser <andrew.doser@oit.edu>
2018-12-06 23:59:10 -0800

    FINALLY

--- CST324_TermProject_RideShare/Forms/form_Admin.Designer.cs
+++ CST324_TermProject_RideShare/Forms/form_Admin.Designer.cs
@@ -48,31 +48,29 @@
             // listb_Users
             // 
             this.listb_Users.FormattingEnabled = true;
-            this.listb_Users.ItemHeight = 16;
-            this.listb_Users.Location = new System.Drawing.Point(12, 60);
-            this.listb_Users.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listb_Users.Location = new System.Drawing.Point(9, 49);
+            this.listb_Users.Margin = new System.Windows.Forms.Padding(2);
             this.listb_Users.Name = "listb_Users";
-            this.listb_Users.Size = new System.Drawing.Size(203, 292);
+            this.listb_Users.Size = new System.Drawing.Size(153, 238);
             this.listb_Users.TabIndex = 0;
             this.listb_Users.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listb_Users_MouseClick);
             // 
             // listb_Requests
             // 
             this.listb_Requests.FormattingEnabled = true;
-            this.listb_Requests.ItemHeight = 16;
-            this.listb_Requests.Location = new System.Drawing.Point(405, 60);
-            this.listb_Requests.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listb_Requests.Location = new System.Drawing.Point(304, 49);
+            this.listb_Requests.Margin = new System.Windows.Forms.Padding(2);
             this.listb_Requests.Name = "listb_Requests";
-            this.listb_Requests.Size = new System.Drawing.Size(203, 292);
+            this.listb_Requests.Size = new System.Drawing.Size(153, 238);
             this.listb_Requests.TabIndex = 2;
             this.listb_Requests.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listb_Requests_MouseClick);
             // 
             // btn_Rider_Update
             // 
-            this.btn_Rider_Update.Location = new System.Drawing.Point(12, 359);
-            this.btn_Rider_Update.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Rider_Update.Location = new System.Drawing.Point(9, 292);
+            this.btn_Rider_Update.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Rider_Update.Name = "btn_Rider_Update";
-            this.btn_Rider_Update.Size = new System.Drawing.Size(77, 23);
+            this.btn_Rider_Update.Size = new System.Drawing.Size(58, 19);
             this.btn_Rider_Update.TabIndex = 3;
             this.btn_Rider_Update.Text = "Update";
             this.btn_Rider_Update.UseVisualStyleBackColor = true;
@@ -80,10 +78,10 @@
             // 
             // btn_Rider_Delete
             // 
-            this.btn_Rider_Delete.Location = new System.Drawing.Point(152, 358);
-            this.btn_Rider_Delete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Rider_Delete.Location = new System.Drawing.Point(114, 291);
+            this.btn_Rider_Delete.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Rider_Delete.Name = "btn_Rider_Delete";
-            this.btn_Rider_Delete.Size = new System.Drawing.Size(61, 23);
+            this.btn_Rider_Delete.Size = new System.Drawing.Size(46, 19);
             this.btn_Rider_Delete.TabIndex = 4;
             this.btn_Rider_Delete.Text = "Delete";
             this.btn_Rider_Delete.UseVisualStyleBackColor = true;
@@ -91,10 +89,10 @@
             // 
             // btn_Rider_Add
             // 
-            this.btn_Rider_Add.Location = new System.Drawing.Point(95, 359);
-            this.btn_Rider_Add.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Rider_Add.Location = new System.Drawing.Point(71, 292);
+            this.btn_Rider_Add.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Rider_Add.Name = "btn_Rider_Add";
-            this.btn_Rider_Add.Size = new System.Drawing.Size(51, 23);
+            this.btn_Rider_Add.Size = new System.Drawing.Size(38, 19);
             this.btn_Rider_Add.TabIndex = 5;
             this.btn_Rider_Add.Text = "Add";
             this.btn_Rider_Add.UseVisualStyleBackColor = true;
@@ -102,97 +100,102 @@
             // 
             // btn_Requests_Add
             // 
-            this.btn_Requests_Add.Location = new System.Drawing.Point(491, 359);
-            this.btn_Requests_Add.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Requests_Add.Location = new System.Drawing.Point(368, 292);
+            this.btn_Requests_Add.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Requests_Add.Name = "btn_Requests_Add";
-            this.btn_Requests_Add.Size = new System.Drawing.Size(51, 23);
+            this.btn_Requests_Add.Size = new System.Drawing.Size(38, 19);
             this.btn_Requests_Add.TabIndex = 11;
             this.btn_Requests_Add.Text = "Add";
             this.btn_Requests_Add.UseVisualStyleBackColor = true;
+            this.btn_Requests_Add.Click += new System.EventHandler(this.btn_Requests_Add_Click);
             // 
             // btn_Requests_Delete
             // 
-            this.btn_Requests_Delete.Location = new System.Drawing.Point(547, 359);
-            this.btn_Requests_Delete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Requests_Delete.Location = new System.Drawing.Point(410, 292);
+            this.btn_Requests_Delete.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Requests_Delete.Name = "btn_Requests_Delete";
-            this.btn_Requests_Delete.Size = new System.Drawing.Size(61, 23);
+            this.btn_Requests_Delete.Size = new System.Drawing.Size(46, 19);
             this.btn_Requests_Delete.TabIndex = 10;
             this.btn_Requests_Delete.Text = "Delete";
             this.btn_Requests_Delete.UseVisualStyleBackColor = true;
+            this.btn_Requests_Delete.Click += new System.EventHandler(this.btn_Requests_Delete_Click);
             // 
             // btn_Requests_Update
             // 
-            this.btn_Requests_Update.Location = new System.Drawing.Point(407, 359);
-            this.btn_Requests_Update.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Requests_Update.Location = new System.Drawing.Point(305, 292);
+            this.btn_Requests_Update.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Requests_Update.Name = "btn_Requests_Update";
-            this.btn_Requests_Update.Size = new System.Drawing.Size(77, 23);
+            this.btn_Requests_Update.Size = new System.Drawing.Size(58, 19);
             this.btn_Requests_Update.TabIndex = 9;
             this.btn_Requests_Update.Text = "Update";
             this.btn_Requests_Update.UseVisualStyleBackColor = true;
-            this.btn_Requests_Update.Click += new System.EventHandler(this.btn_Requests_Update_Click);
             // 
             // tb_Details_User
             // 
-            this.tb_Details_User.Location = new System.Drawing.Point(221, 60);
-            this.tb_Details_User.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_Details_User.Location = new System.Drawing.Point(166, 49);
+            this.tb_Details_User.Margin = new System.Windows.Forms.Padding(2);
             this.tb_Details_User.Multiline = true;
             this.tb_Details_User.Name = "tb_Details_User";
             this.tb_Details_User.ReadOnly = true;
-            this.tb_Details_User.Size = new System.Drawing.Size(161, 292);
+            this.tb_Details_User.Size = new System.Drawing.Size(122, 238);
             this.tb_Details_User.TabIndex = 12;
             // 
             // lb_List_Users
             // 
             this.lb_List_Users.AutoSize = true;
-            this.lb_List_Users.Location = new System.Drawing.Point(13, 37);
+            this.lb_List_Users.Location = new System.Drawing.Point(10, 30);
+            this.lb_List_Users.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_List_Users.Name = "lb_List_Users";
-            this.lb_List_Users.Size = new System.Drawing.Size(87, 17);
+            this.lb_List_Users.Size = new System.Drawing.Size(65, 13);
             this.lb_List_Users.TabIndex = 13;
             this.lb_List_Users.Text = "List of Users";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(219, 39);
+            this.label2.Location = new System.Drawing.Point(164, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(160, 17);
+            this.label2.Size = new System.Drawing.Size(121, 13);
             this.label2.TabIndex = 14;
             this.label2.Text = "Details of Selected User";
             // 
             // lb_List_Requests
             // 
             this.lb_List_Requests.AutoSize = true;
-            this.lb_List_Requests.Location = new System.Drawing.Point(403, 37);
+            this.lb_List_Requests.Location = new System.Drawing.Point(302, 30);
+            this.lb_List_Requests.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_List_Requests.Name = "lb_List_Requests";
-            this.lb_List_Requests.Size = new System.Drawing.Size(110, 17);
+            this.lb_List_Requests.Size = new System.Drawing.Size(83, 13);
             this.lb_List_Requests.TabIndex = 15;
             this.lb_List_Requests.Text = "List of Requests";
             // 
             // lb_Details_Requests
             // 
             this.lb_Details_Requests.AutoSize = true;
-            this.lb_Details_Requests.Location = new System.Drawing.Point(612, 39);
+            this.lb_Details_Requests.Location = new System.Drawing.Point(459, 32);
+            this.lb_Details_Requests.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_Details_Requests.Name = "lb_Details_Requests";
-            this.lb_Details_Requests.Size = new System.Drawing.Size(183, 17);
+            this.lb_Details_Requests.Size = new System.Drawing.Size(139, 13);
             this.lb_Details_Requests.TabIndex = 17;
             this.lb_Details_Requests.Text = "Details of Selected Request";
             // 
             // tb_Details_Requests
             // 
-            this.tb_Details_Requests.Location = new System.Drawing.Point(615, 60);
-            this.tb_Details_Requests.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_Details_Requests.Location = new System.Drawing.Point(461, 49);
+            this.tb_Details_Requests.Margin = new System.Windows.Forms.Padding(2);
             this.tb_Details_Requests.Multiline = true;
             this.tb_Details_Requests.Name = "tb_Details_Requests";
             this.tb_Details_Requests.ReadOnly = true;
-            this.tb_Details_Requests.Size = new System.Drawing.Size(161, 292);
+            this.tb_Details_Requests.Size = new System.Drawing.Size(122, 238);
             this.tb_Details_Requests.TabIndex = 16;
             // 
             // btn_Finish
             // 
-            this.btn_Finish.Location = new System.Drawing.Point(719, 415);
-            this.btn_Finish.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Finish.Location = new System.Drawing.Point(539, 337);
+            this.btn_Finish.Margin = new System.Windows.Forms.Padding(2);
             this.btn_Finish.Name = "btn_Finish";
-            this.btn_Finish.Size = new System.Drawing.Size(75, 23);
+            this.btn_Finish.Size = new System.Drawing.Size(56, 19);
             this.btn_Finish.TabIndex = 18;
             this.btn_Finish.Text = "Finish";
             this.btn_Finish.UseVisualStyleBackColor = true;
@@ -200,9 +203,9 @@
             // 
             // form_Admin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btn_Finish);
             this.Controls.Add(this.lb_Details_Requests);
             this.Controls.Add(this.tb_Details_Requests);
@@ -218,7 +221,7 @@
             this.Controls.Add(this.btn_Rider_Update);
             this.Controls.Add(this.listb_Requests);
             this.Controls.Add(this.listb_Users);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "form_Admin";
             this.Text = "form_Admin";
             this.ResumeLayout(false);
---
+++ CST324_TermProject_RideShare/Forms/form_admin_add_request.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace CST324_TermProject_RideShare.Forms
+{
+    partial class form_admin_add_request
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_VehicleType = new System.Windows.Forms.Label();
+            this.tb_VehicleType = new System.Windows.Forms.TextBox();
+            this.btn_Finish = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.lb_RiderID = new System.Windows.Forms.Label();
+            this.lb_Admin = new System.Windows.Forms.Label();
+            this.lb_Location = new System.Windows.Forms.Label();
+            this.tb_RiderID = new System.Windows.Forms.TextBox();
+            this.tb_Admin = new System.Windows.Forms.TextBox();
+            this.tb_Location = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lb_VehicleType
+            // 
+            this.lb_VehicleType.AutoSize = true;
+            this.lb_VehicleType.Location = new System.Drawing.Point(8, 67);
+            this.lb_VehicleType.Name = "lb_VehicleType";
+            this.lb_VehicleType.Size = new System.Drawing.Size(69, 13);
+            this.lb_VehicleType.TabIndex = 39;
+            this.lb_VehicleType.Text = "Desired Time";
+            // 
+            // tb_VehicleType
+            // 
+            this.tb_VehicleType.Location = new System.Drawing.Point(85, 64);
+            this.tb_VehicleType.Name = "tb_VehicleType";
+            this.tb_VehicleType.Size = new System.Drawing.Size(165, 20);
+            this.tb_VehicleType.TabIndex = 36;
+            // 
+            // btn_Finish
+            // 
+            this.btn_Finish.Location = new System.Drawing.Point(62, 150);
+            this.btn_Finish.Name = "btn_Finish";
+            this.btn_Finish.Size = new System.Drawing.Size(75, 23);
+            this.btn_Finish.TabIndex = 33;
+            this.btn_Finish.Text = "Finish";
+            this.btn_Finish.UseVisualStyleBackColor = true;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(143, 150);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(107, 23);
+            this.btn_add.TabIndex = 32;
+            this.btn_add.Text = "Add Request";
+            this.btn_add.UseVisualStyleBackColor = true;
+            // 
+            // lb_RiderID
+            // 
+            this.lb_RiderID.AutoSize = true;
+            this.lb_RiderID.Location = new System.Drawing.Point(8, 93);
+            this.lb_RiderID.Name = "lb_RiderID";
+            this.lb_RiderID.Size = new System.Drawing.Size(59, 13);
+            this.lb_RiderID.TabIndex = 31;
+            this.lb_RiderID.Text = "Driver ETA";
+            // 
+            // lb_Admin
+            // 
+            this.lb_Admin.AutoSize = true;
+            this.lb_Admin.Location = new System.Drawing.Point(8, 41);
+            this.lb_Admin.Name = "lb_Admin";
+            this.lb_Admin.Size = new System.Drawing.Size(59, 13);
+            this.lb_Admin.TabIndex = 29;
+            this.lb_Admin.Text = "Credit Card";
+            // 
+            // lb_Location
+            // 
+            this.lb_Location.AutoSize = true;
+            this.lb_Location.Location = new System.Drawing.Point(8, 15);
+            this.lb_Location.Name = "lb_Location";
+            this.lb_Location.Size = new System.Drawing.Size(48, 13);
+            this.lb_Location.TabIndex = 28;
+            this.lb_Location.Text = "Location";
+            // 
+            // tb_RiderID
+            // 
+            this.tb_RiderID.Location = new System.Drawing.Point(85, 90);
+            this.tb_RiderID.Name = "tb_RiderID";
+            this.tb_RiderID.Size = new System.Drawing.Size(165, 20);
+            this.tb_RiderID.TabIndex = 27;
+            // 
+            // tb_Admin
+            // 
+            this.tb_Admin.Location = new System.Drawing.Point(85, 38);
+            this.tb_Admin.Name = "tb_Admin";
+            this.tb_Admin.Size = new System.Drawing.Size(165, 20);
+            this.tb_Admin.TabIndex = 25;
+            // 
+            // tb_Location
+            // 
+            this.tb_Location.Location = new System.Drawing.Point(85, 12);
+            this.tb_Location.Name = "tb_Location";
+            this.tb_Location.Size = new System.Drawing.Size(165, 20);
+            this.tb_Location.TabIndex = 24;
+            this.tb_Location.TextChanged += new System.EventHandler(this.tb_Location_TextChanged);
+            // 
+            // form_admin_add_request
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 197);
+            this.Controls.Add(this.lb_VehicleType);
+            this.Controls.Add(this.tb_VehicleType);
+            this.Controls.Add(this.btn_Finish);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.lb_RiderID);
+            this.Controls.Add(this.lb_Admin);
+            this.Controls.Add(this.lb_Location);
+            this.Controls.Add(this.tb_RiderID);
+            this.Controls.Add(this.tb_Admin);
+            this.Controls.Add(this.tb_Location);
+            this.Name = "form_admin_add_request";
+            this.Text = "form_admin_add_request";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_VehicleType;
+        private System.Windows.Forms.TextBox tb_VehicleType;
+        private System.Windows.Forms.Button btn_Finish;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Label lb_RiderID;
+        private System.Windows.Forms.Label lb_Admin;
+        private System.Windows.Forms.Label lb_Location;
+        private System.Windows.Forms.TextBox tb_RiderID;
+        private System.Windows.Forms.TextBox tb_Admin;
+        private System.Windows.Forms.TextBox tb_Location;
+    }
+}
